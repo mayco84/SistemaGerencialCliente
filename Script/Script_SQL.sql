8d973c995cbbef32daa4679bb383bd7a04cdad21
mayco84 <55723925+mayco84@users.noreply.github.com>
2025-01-10 22:03:18 -0300

    Add files via upload

--- Script/Script_SQL.sql
+++ Script/Script_SQL.sql
@@ -1,23 +1,51 @@
-
-
--- Criação da Base de Dados (caso ainda não exista)
-ATTACH DATABASE 'BaseDeDados.db' AS BaseDeDados;
-
 CREATE TABLE Cliente (
-    id             INTEGER       PRIMARY KEY AUTOINCREMENT,  -- ID único para cada cliente, auto incrementado
-    TipoPessoa     VARCHAR (1)   NOT NULL,                    -- Tipo de pessoa (Física ou Jurídica), não pode ser nulo
-    FisicaJuridica VARCHAR (1)   NOT NULL,                    -- Indica se a pessoa é Física ou Jurídica, não pode ser nulo
-    NomeCompleto   VARCHAR (100) NOT NULL,                    -- Nome completo do cliente, não pode ser nulo
-    DataNascimento DATE          NOT NULL,                    -- Data de nascimento do cliente, não pode ser nulo
-    CPF            VARCHAR (20)  NOT NULL UNIQUE,             -- CPF do cliente, único, não pode ser nulo
-    RG             VARCHAR (20)  NOT NULL,                    -- RG do cliente, não pode ser nulo
-    Email          VARCHAR (100) NOT NULL,                    -- E-mail do cliente, não pode ser nulo
-    Telefone       VARCHAR (20),                              -- Telefone do cliente (opcional)
-    Endereco       VARCHAR (200),                             -- Endereço do cliente (opcional)
-    Bairro         VARCHAR (200),                             -- Bairro do cliente (opcional)
-    Cidade         VARCHAR (200),                             -- Cidade do cliente (opcional)
-    CEP            VARCHAR (20),                              -- CEP do cliente (opcional)
-    numero         VARCHAR (20),                              -- Número do endereço (opcional)
-    complemento    VARCHAR (20),                              -- Complemento do endereço (opcional)
-    uf             VARCHAR (2)                                -- UF (Estado) do cliente (opcional)
+    id             INTEGER       PRIMARY KEY AUTOINCREMENT,
+    TipoPessoa     VARCHAR (1)   NOT NULL,
+    FisicaJuridica VARCHAR (1)   NOT NULL,
+    NomeCompleto   VARCHAR (100) NOT NULL,
+    DataNascimento DATE          NOT NULL,
+    CPF            VARCHAR (20)  NOT NULL
+                                 UNIQUE,
+    RG             VARCHAR (20)  NOT NULL,
+    Email          VARCHAR (100) NOT NULL,
+    Telefone       VARCHAR (20),
+    Endereco       VARCHAR (200),
+    Bairro         VARCHAR (200),
+    Cidade         VARCHAR (200),
+    CEP            VARCHAR (20),
+    numero         VARCHAR (20),
+    complemento    VARCHAR (20),
+    uf             VARCHAR (2) 
+);
+
+CREATE TABLE Pedido (
+    id         INTEGER PRIMARY KEY,
+    DataPedido DATE    NOT NULL,
+    IdCliente  INTEGER REFERENCES Cliente (id) 
+                       NOT NULL,
+    Total      REAL
+);
+
+CREATE TABLE PedidoItens (
+    id              INTEGER PRIMARY KEY,
+    idPedido        INTEGER REFERENCES Pedido (id),
+    Quantidade      REAL,
+    idUnidadeMedida INTEGER REFERENCES UnidadeMedida (id),
+    Valor           REAL    NOT NULL,
+    Total           REAL    NOT NULL,
+    idProduto       INTEGER REFERENCES Produtos (id) 
+);
+CREATE TABLE Produtos (
+    id               INTEGER      PRIMARY KEY,
+    idUnidadeMedida  INTEGER      REFERENCES UnidadeMedida (id),
+    DataCadastro     DATE         NOT NULL,
+    DescricaoProduto VARCHAR (50) NOT NULL,
+    Valor            FLOAT        NOT NULL
+);
+
+CREATE TABLE UnidadeMedida (
+    id                     INTEGER      PRIMARY KEY,
+    DescricaoUnidadeMedida VARCHAR (50) NOT NULL,
+    SiglaUnidadeMedida     VARCHAR (4)  NOT NULL,
+    DataCadastro           DATE         NOT NULL
 );
